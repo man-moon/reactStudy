47513dc36c2a518ebc0c444e50429016d0bab63f
이 영민 <andy3014@ajou.ac.kr>
2022-04-29 03:18:09 +0000

    Update LikeMusicList.js

--- src/LikeMusicList.js
+++ src/LikeMusicList.js
@@ -5,18 +5,23 @@ import {Card, CardContent, CardActions, Typography, IconButton} from '@material-
 import firebase from './firebase';
 
 class LikeMusicList extends React.Component{
- 
-  render () {
+  
+  state = {
+    list: []
+  }
+  
+  componentDidMount() {
     let db = firebase.firestore();
     let snapshot = db.collection("likes");
-    let list = []
 
-    snapshot.get().then(function(querySnapshot) {
+    snapshot.get().then((querySnapshot) => {
       if (querySnapshot) {
-        querySnapshot.forEach(function(item){
-          //doc.id로 id 접근 가능
+        querySnapshot.forEach((item) => {
           fetch(`https://itunes.apple.com/lookup?id=${item.id}&entity=album`).then(r => r.json()).then(r => {
-            list.push(r.results[0]);
+            this.setState({
+              list: r.results[0]
+            })
+            console.log(r.results[0]);
           }).catch(e => console.log(e));
         })
       }
@@ -26,20 +31,24 @@ class LikeMusicList extends React.Component{
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
-    //console.log(list);
 
-    ///*** 여기서부터 list 인식을 못 함.. ***/
+  }
+ 
+  render () {
+
     return (
       <div>
-          {list.map(item => {
+          {this.state.list.results&&this.state.list.results.map(item => {
+            return (
             <Card key={item.collectionId} className={classes.card}>
               <CardContent>
                   <Typography variant="subtitle1"> {item.artistName}</Typography>
                   <Typography variant="subtitle2"> {item.collectionCensoredName}</Typography>
               </CardContent>
               <img src = {item.collectionViewUrl} alt = {item.collectionName}></img>
-            </Card>
+            </Card>)
           })}
+        {console.log(this.state.list)}
       </div>
     );
   };
